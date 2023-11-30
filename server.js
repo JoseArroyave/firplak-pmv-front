937eb59f99174e9a222c3ead9114678affc7b4b4
JoseArroyave <poer0601@hotmail.com>
2023-11-30 00:18:11 -0500

    quitar __dirname

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/firplak-front/browser'));
+app.use(express.static('/dist/firplak-front/browser'));
 app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname + '/dist/firplak-front/browser/index.html'));
+  res.sendFile(path.join('/dist/firplak-front/browser/index.html'));
 });
 app.listen(process.env.PORT || 8080); 
