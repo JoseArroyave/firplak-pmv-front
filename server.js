ba1a479f80bcf6fad8a3988965020c0422b09d72
JoseArroyave <poer0601@hotmail.com>
2023-11-30 00:03:42 -0500

    dist/firplak-front

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/<firplak-front>'));
+app.use(express.static(__dirname + '/dist/firplak-front'));
 app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname + '/dist/browser/<firplak-front > /index.html'));
+  res.sendFile(path.join(__dirname + '/dist/firplak-front/index.html'));
 });
 app.listen(process.env.PORT || 8080);
