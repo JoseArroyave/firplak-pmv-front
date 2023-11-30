2a62d9149656d901724f97286deaeb0f8caf99cc
JoseArroyave <poer0601@hotmail.com>
2023-11-29 21:14:22 -0500

    terminar pod

--- src/app/components/pod/pod.component.ts
+++ src/app/components/pod/pod.component.ts
@@ -1,15 +1,50 @@
+import { ReactiveFormsModule, UntypedFormBuilder, UntypedFormGroup, Validators } from '@angular/forms';
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-pod',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './pod.component.html',
   styleUrl: './pod.component.scss'
 })
 export class PodComponent {
 
   @Input() id_guia: string = '';
+  public podForm!: UntypedFormGroup;
+
+  constructor(
+    private formBuilder: UntypedFormBuilder,
+  ) { }
+
+  ngOnInit(): void {
+    this.initForms();
+  }
+
+  initForms = () => {
+    this.podForm = this.formBuilder.group({
+      guia: [null, [Validators.required]],
+      documentos: [null, [Validators.required]],
+    });
+  }
+
+  savePDO = () => {
+    if (this.podForm.valid) {
+      Swal.fire({
+        title: "",
+        text: "POD guardado correctamente",
+        icon: "success"
+      });
+      this.podForm.reset();
+    } else {
+      Swal.fire({
+        title: "",
+        text: "Ingresa los datos",
+        icon: "error"
+      });
+    }
+  }
 
 }
