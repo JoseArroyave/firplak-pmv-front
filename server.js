3cd18755b87a08dd820b905a11d0d0113332831f
JoseArroyave <poer0601@hotmail.com>
2023-11-29 23:58:20 -0500

    espacio server.js

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ const path = require('path');
 const app = express();
 app.use(express.static(__dirname + '/dist/<firplak-front>'));
 app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname + '/dist/<firplak-front>/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/<firplak-front > /index.html'));
 });
 app.listen(process.env.PORT || 8080);
