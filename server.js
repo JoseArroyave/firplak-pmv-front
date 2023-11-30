7cde01beedd21c2f7e637db1bfcec457bf502550
JoseArroyave <poer0601@hotmail.com>
2023-11-30 00:27:59 -0500

    quitar editar server.js

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static('/dist/firplak-front/browser'));
-app.get('/*', function (req, res) {
-  res.sendFile(path.join('/dist/firplak-front/browser/index.html'));
+app.use(express.static('./dist/firplak-front/browser'));
+app.get('/*', (req, res) => {
+  res.sendFile('index.html', { root: 'dist/firplak-front/browser/' });
 });
 app.listen(process.env.PORT || 8080); 
